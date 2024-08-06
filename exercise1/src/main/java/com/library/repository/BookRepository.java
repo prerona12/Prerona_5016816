9fbf4d085f83b2d9d0885969ca095d8717699da1
prerona12 <147043147+prerona12@users.noreply.github.com>
2024-08-06 13:39:51 +0530

    Delete exercise1 directory

--- exercise1/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    public void fetchBooks() {
-        // Data access logic here
-        System.out.println("Fetching books from repository...");
-    }
-}
