41fc1292102e5ae9adeb6907f19647aa4a0e01d3
prerona12 <147043147+prerona12@users.noreply.github.com>
2024-08-06 13:41:03 +0530

    Delete exercise2 directory

--- exercise2/README.md
+++
@@ -1,5 +0,0 @@
-### Output:
-Service is performing operation using repository.
-<br />
-Repository is performing its task.
-
--- exercise2/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    // Example method to demonstrate functionality
-    public void performRepositoryTask() {
-        System.out.println("Repository is performing its task.");
-    }
-}
